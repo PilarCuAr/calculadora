c193480553da3c3292819547c28cf4b24672afe6
PilarCuAr <tauro10cuen@gmail.com>
2022-09-02 19:59:07 -0500

    Initial Commit

---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by MAC on 02/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
