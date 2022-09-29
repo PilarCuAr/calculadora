fa7942f73269243b12c6116380dc19db319f3b1d
PilarCuAr <tauro10cuen@gmail.com>
2022-09-28 21:00:19 -0500

    primer commit

--- Testing/ContentView.swift
+++ Testing/ContentView.swift
@@ -6,16 +6,57 @@
 //
 
 import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+ 
+func sum(_ x: Int, _ y: Int) -> Int {
+  x+y
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct Movie: Decodable {
+ var name: String
+ var releaseDate: Date
+ 
+ enum CodingKeys: String, CodingKey {
+   case movie = "movie_name"
+ 
+   case releaseMonth = "release_month"
+   case releaseDay = "release_day"
+   case releaseYear = "release_year"
+}
+ 
+ init(from decoder: Decoder) throws {
+   let values = try decoder.container(keyedBy: CodingKeys.self)
+   name = try values.decode(String.self, forKey: .movie)
+   let releaseMonth = try values.decode(String.self, forKey: .releaseMonth)
+   let releaseDay = try values.decode(String.self, forKey: .releaseDay)
+   let releaseYear = try values.decode(String.self, forKey: .releaseYear)
+ 
+   let dateString = "\(releaseMonth)/\(releaseDay)/\(releaseYear)"
+   let dateFormatter = DateFormatter()
+   dateFormatter.dateFormat = "MM/dd/yy"
+ 
+   releaseDate = dateFormatter.date(from: dateString) ?? Date()
+ }
+}
+struct ContentView: View {
+ 
+  @State private var number1: String = ""
+  @State private var number2: String = ""
+ 
+  var body: some View {
+    VStack {
+      Form {
+        TextField("Number 1", text: $number1)
+        TextField("Number 2", text: $number2)
+ 
+        Text("Sum: \(sum((Int(number1) ?? 0), (Int(number2) ?? 0)))")
+        Spacer()
+      }
     }
+  }
+}
+ 
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
 }
--- TestingTests/TestingTests.swift
+++ TestingTests/TestingTests.swift
@@ -18,14 +18,46 @@ class TestingTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    /* func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }*/
+    
+    func testSum() throws {
+      XCTAssert(sum(2, 4) == 6)
+      XCTAssert(sum(3, 0) == 3)
+      XCTAssert(sum(-3, 0) == -3)
     }
-
+    
+    func testMovie() throws {
+     
+      // 1
+      let bundle = Bundle(for: type(of: self))
+     
+      guard let url = bundle.url(forResource: "data", withExtension: "json") else {
+          //Creado mal el data.json
+        XCTFail("Missing file: data.json")
+        return
+      }
+     
+      let json = try Data(contentsOf: url)
+      let decoder = JSONDecoder()
+     
+      // 2
+      let movie = try decoder.decode(Movie.self, from: json)
+     
+      XCTAssertNotNil(movie)
+      XCTAssertEqual(movie.name, "Titanic")
+     
+      let dateFormatter = DateFormatter()
+      dateFormatter.dateFormat = "MM/dd/yy"
+      let date = dateFormatter.date(from: "12/19/1997")
+      XCTAssertEqual(movie.releaseDate, date)
+    }
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
